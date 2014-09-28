8f4e208f2a5e49b3178b0fdcf68aef906831fad5
Erin Hinkle <erinhinx5@gmail.com>
2014-09-28 14:09:35 -0400

    whew!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-	$('.initiate').on('click', function() {
+	$('.user-input').on("keypress", function(event)) {
+		if( event.which == 13) {
+			numCheck;
+		};
+	});
+	$('.initiate').on('click', numCheck);
+
+//function for making sure user enters a number
+function numCheck() {
+	var userNumber = +$('user-input').val();
+	console.log(userNumber);
+	if(isNaN(userNumber)) {
+		alert("Please choose a NUMBER!")
+	} else if (userNumber %1 !=0) {
+		alert("Please choose an INTEGER aka WHOLE NUMBER!")
+	} else {
+		runFizzBuzz(userNumber);
+	}
+}; 
+
+//function that runs fizzbuzz
+var runFizzBuzz = function() {
 		var sum = 0;
 		for (i=1; i<=999; i++) {
 			if ((i%3) === 0) {
